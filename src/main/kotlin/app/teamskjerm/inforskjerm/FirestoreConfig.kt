b1971b61249960013becf6706fe37b0d19203ca9
Olav Hermansen <Olav.Hermansen@spk.no>
2025-01-15 14:58:04 +0100

    Prøver å lese med file:

--- src/main/kotlin/app/teamskjerm/inforskjerm/FirestoreConfig.kt
+++ src/main/kotlin/app/teamskjerm/inforskjerm/FirestoreConfig.kt
@@ -6,28 +6,19 @@ import com.google.cloud.firestore.FirestoreOptions
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import java.io.FileInputStream
-import java.nio.file.Files
-import java.nio.file.Paths
 
 @Configuration
 class FirestoreConfig {
 
-    @Value("\${secrets.path:/secrets/teamskjerm-firestore}")
-    private val secretsPath: String? = null
-
-    @Throws(Exception::class)
-    fun firestoreSecret(): String {
-        val filePath = Paths.get(secretsPath)
-        return Files.readString(filePath)
-    }
+    @Value("file:/secrets/teamskjerm-firestore")
+    private val secretContent: String? = null
 
     @Bean
     fun firestoreClient(): Firestore {
 //        val credentialsPath = "src/main/resources/monitor-404-firestore-credentials.json"
 //        val credentials = GoogleCredentials.fromStream(FileInputStream(credentialsPath))
 
-        val credentials = GoogleCredentials.fromStream(FileInputStream(firestoreSecret()))
+        val credentials = GoogleCredentials.fromStream(secretContent!!.byteInputStream())
 
         val options = FirestoreOptions.newBuilder()
             .setCredentials(credentials)
